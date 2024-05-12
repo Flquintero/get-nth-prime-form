fba208dbb15f1c0ee8db64718dede96fd13c3553
Francisco Quintero Abbo <flquintero1@gmail.com>
2024-05-11 23:45:36 -0400

    feat: added component to test package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  Flex,
+  Text,
+  Grid,
+  Button,
+  Card,
+  Box,
+  TextField,
+  Heading,
+} from "@radix-ui/themes";
+import { getNthPrime } from "nth-prime-retriever";
 
-function App() {
+const App = () => {
+  const [nthPosition, setNthPosition] = useState<number | undefined>();
+  const [nthNumber, setNthNumber] = useState<number | null>();
+  const handleSetNthPositon = (nthPosition: number): void => {
+    if (nthNumber) setNthNumber(null);
+    setNthPosition(nthPosition);
+  };
+  const handleSetNthNumber = (): void => {
+    setNthNumber(getNthPrime(nthPosition as number));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box maxWidth="500px" p="3" mx="auto" mt="100px">
+      <Card size="2">
+        <Flex direction="column" gap="3">
+          <Heading as="h1" size="5" my="2" align="center">
+            Get Nth Prime Number
+          </Heading>
+          <Grid gap="2">
+            <form
+              onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+                e.preventDefault();
+                handleSetNthNumber();
+              }}
+            >
+              <TextField.Root
+                size="3"
+                type="number"
+                placeholder="Enter nth position"
+                onInput={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleSetNthPositon(parseInt(e.target.value))
+                }
+              />
+            </form>
+          </Grid>
+          <Grid columns="1">
+            <Button type="submit" size="3" my="3">
+              Get Number
+            </Button>
+            {nthNumber && (
+              <Text as="div" size="5" mt="2" align="center">
+                For the {nthPosition} position the prime number is {nthNumber}
+              </Text>
+            )}
+          </Grid>
+        </Flex>
+      </Card>
+    </Box>
   );
-}
+};
 
 export default App;
